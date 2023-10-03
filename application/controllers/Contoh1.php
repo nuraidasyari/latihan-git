15f6552fd23fb9192fb6ca04c961982fbb9b7271
nuraidasyari <nuraidasyari230104@gmail.com>
2023-10-03 17:20:46 +0700

    penambahan. htaccess

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+{
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama  : Saya Nur Aida Syari
+         Nim   : 19220363
+         Kelas : 19.3A.26
+         Alamat : Kp Kecil 
+         Hobi   : Memasak";
+    }
+}       
---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan1</title>
+</head>
+<body>
+    Halo Kawan.. Yuk Kita Belajar Web Programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+    $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
